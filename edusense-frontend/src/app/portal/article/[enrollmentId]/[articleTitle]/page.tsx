b12acaa3ee323c0235601fb8a237333a94929294
Divyansh Rajesh Jain <drajeshjain@ucdavis.edu>
2025-04-27 14:34:57 -0700

    Added Functionality to render SVG generated images by LLM

--- edusense-frontend/src/app/portal/article/[enrollmentId]/[articleTitle]/page.tsx
+++ edusense-frontend/src/app/portal/article/[enrollmentId]/[articleTitle]/page.tsx
@@ -2,11 +2,44 @@
 import { useState, useEffect } from 'react';
 import { AI_ENDPOINT, API_PREFIX } from "../../../../global";
 import { IArticleResponse, IJourney, IJourneyResponse } from '../../../../typedef';
-import { httpPost } from '../../../../utils';
+import { httpGet, httpPost } from '../../../../utils';
 import { useParams } from 'next/navigation';
+import { useCustomProp } from '@/app/portal/layout';
 
 import ReactMarkdown from "react-markdown"
-import { useCustomProp } from '@/app/portal/layout';
+import rehypeRaw from "rehype-raw"; 
+
+interface ILatexToSvgResponse {
+    svg: string
+}
+
+
+const MarkdownViewer = ({ content }: { content: string }) => {
+  return (
+    <div className="prose max-w-none">
+      <ReactMarkdown
+        rehypePlugins={[rehypeRaw]}  // Allow raw HTML (SVG)
+        components={{
+          code({ node, className, children, ...props }) {
+            // Check if it's an SVG block
+            if (className && className.includes('language-svg')) {
+              // Render the SVG content directly
+              const svgContent = children?.toString();
+              return <div dangerouslySetInnerHTML={{ __html: svgContent || "" }} />;
+            }
+            return (
+              <pre className={className}>
+                <code>{children}</code>
+              </pre>
+            );
+          },
+        }}
+      >
+        {content}
+      </ReactMarkdown>
+    </div>
+  );
+};
 
 export default function CourseRoadmap() {
     const params = useParams();
@@ -38,7 +71,43 @@ export default function CourseRoadmap() {
 
         requestResponse.then((response) => {
             console.log(response.data);
-            setArticle(response.data.data.article);
+            const tikzBlocks = [...response.data.data.article.matchAll(/\\begin\{tikzpicture\}[\s\S]*?\\end\{tikzpicture\}/g)];
+
+            const svgPromises = tikzBlocks.map((block, i) => {
+                let latex = block[0];
+                const fullLatexDocument = `
+                    \\documentclass{standalone}
+                    \\usepackage{tikz}
+                    \\begin{document}
+                    ${latex} 
+                    \\end{document}
+                    `;
+                const API_URL = API_PREFIX + AI_ENDPOINT;
+
+                let queryParams = {
+                    section: "latex_to_svg",
+                    latex_source: fullLatexDocument 
+                }
+
+                const requestResponse = httpGet<ILatexToSvgResponse>(API_URL, queryParams);
+                return requestResponse;
+            })
+            let articleProcessed = response.data.data.article;
+            Promise.all(svgPromises).then((svgResponses) => {
+
+                tikzBlocks.forEach((block, i) => {
+                    const svgString = svgResponses[i].data.svg;
+
+                    const imgTag = `<div>${svgString}</div>`
+
+                    console.log(imgTag);
+
+                    articleProcessed = articleProcessed.replace(block[0], imgTag);
+                })
+            })
+
+            setArticle(articleProcessed);
+
             layoutProps.setPageContext(response.data.data.article);
             layoutProps.setEnrollmentId(parseInt(enrollmentId));
         })
@@ -47,7 +116,7 @@ export default function CourseRoadmap() {
     const renderArticle = () => {
         return (
             <div className="relative w-full flex flex-col px-4 text-black">
-                <ReactMarkdown>{article}</ReactMarkdown>
+                <MarkdownViewer content={article} />
             </div>
         )
     }
