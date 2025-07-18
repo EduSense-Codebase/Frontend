b71857f81e3eff091eccae7aefe2bec483f56c9b
Soham Kolhatkar <43106935+sohamk0524@users.noreply.github.com>
2025-07-17 22:49:39 -0700

    Soham chat intro (#25)
    * chatbot intro messages
    
    * matching activity fix

--- edusense-frontend/src/app/global.ts
+++ edusense-frontend/src/app/global.ts
@@ -6,8 +6,5 @@ export const AUTH_ENDPOINT = 'api/auth/';
 export const COURSE_ENDPOINT = 'api/course/';
 export const AI_ENDPOINT = 'api/generate-content/';
 
-export const OPENAI_API_KEY = 'AIzaSyD9GFGe8K0zXg5OOBP26DphwWYHJgC9dA4';
-export const OPENAI_BASE_URL = 'https://generativelanguage.googleapis.com/v1beta/openai/';
-export const OPENAI_MODEL = 'gemini-2.0-flash';
 export const QUIZ_LENGTH = 5;
 export const TEST_LENGTH = 10;
