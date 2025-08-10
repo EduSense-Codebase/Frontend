bdab96183b5aa1e1441b15e92dfa9e533b3b4206
natalievyh <natalievhoang@gmail.com>
2025-08-10 13:31:43 -0700

    text component to render markdown nicely

---
+++ edusense-frontend/src/app/ui_components/Text/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import './Text.scss';
+
+interface TextProps {
+  content: string;
+}
+
+const Text: React.FC<TextProps> = ({ content }) => {
+  return (
+    <div className="text">
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+      >
+        {content}
+      </ReactMarkdown>
+    </div>
+  );
+};
+
+export default Text;
