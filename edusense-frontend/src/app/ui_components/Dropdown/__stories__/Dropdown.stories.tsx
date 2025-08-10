b44d89a7e0be08c41cc7c988e50ff5b7da35b7eb
natalievyh <natalievhoang@gmail.com>
2025-08-09 17:18:57 -0700

    dropdown component

---
+++ edusense-frontend/src/app/ui_components/Dropdown/__stories__/Dropdown.stories.tsx
@@ -0,0 +1,33 @@
+import type { StoryObj } from '@storybook/nextjs-vite';
+
+import React, { useState } from 'react';
+
+import Dropdown from '../index'
+
+const meta = {
+    title: 'Component/Dropdown',
+    component: Dropdown,
+}
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+    args: {
+        options: ['Option 1', 'Option 2', 'Option 3'],
+        value: "Choose an option",
+        onChange: (value: string) => console.log(value),
+        placeholder: "Choose an option",
+    },
+    render: (args) => {
+        const [value, setValue] = useState('');
+        return (
+            <Dropdown
+                {...args}
+                value={value}
+                onChange={(newValue) => setValue(newValue)}
+            />
+        );
+    },
+};
