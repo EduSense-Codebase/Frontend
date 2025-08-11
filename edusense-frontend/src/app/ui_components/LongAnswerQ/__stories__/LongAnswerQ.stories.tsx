116b7c8332da91bd5dfd2bfde3de9dd488fa5ca5
natalievyh <natalievhoang@gmail.com>
2025-08-10 22:14:35 -0700

    long answer question component

---
+++ edusense-frontend/src/app/ui_components/LongAnswerQ/__stories__/LongAnswerQ.stories.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useState } from 'react';
+import type { StoryObj, Meta } from '@storybook/nextjs-vite';
+import LongAnswerQ, { Mode } from '../index';
+
+const meta: Meta<typeof LongAnswerQ> = {
+  title: 'Component/LongAnswerQ',
+  component: LongAnswerQ,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof LongAnswerQ>;
+
+export const Primary: Story = {
+  render: () => {
+    const [mode, setMode] = useState<Mode>('view');
+    const [question, setQuestion] = useState('What is the meaning of life?');
+    const [description, setDescription] = useState('Description/Instructions');
+    const [isRequired, setIsRequired] = useState(false);
+
+    const handleToggleRequired = (required: boolean) => {
+        setIsRequired(required);
+    };
+
+    return (
+      <LongAnswerQ
+        mode={mode}
+        question={question}
+        onChangeQuestion={setQuestion}
+        description={description}
+        onChangeDescription={setDescription}
+        isRequired={isRequired}
+        onToggleRequired={handleToggleRequired}
+        onEdit={() => setMode('edit')}
+        onSave={() => setMode('view')}
+      />
+    );
+  },
+};
